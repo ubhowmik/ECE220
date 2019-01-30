ded4dd669bd92b29e2fe50a7202dff9812bf2d1f
ubhowmik <46545523+ubhowmik@users.noreply.github.com>
2019-01-30 09:24:45 -0600

    Add files via upload

--- Calculator_test.asm
+++ Calculator_test.asm
@@ -293,7 +293,7 @@ NewlineChar  .FILL     x000A
 ; This routine clears the stack by resetting the stack pointer (R6).
 ;
 OpClear      LEA       R6,StackBase  ; Initialize the Stack.
-             ADD       R6,R6,#1      ; R6 is stack pointer
+             ADD       R6,R6,#-1      ; R6 is stack pointer
              JSR     NewCommand
 
  ;
